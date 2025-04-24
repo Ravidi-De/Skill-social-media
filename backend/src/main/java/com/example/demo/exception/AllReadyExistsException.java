3e5575775cd796bb795f2e38f826bbba05af6c32
Ravidi-De <it21806636@my.sliit.lk>
2025-04-24 11:04:31 +0530

    Implement AllReadyExists exception

---
+++ backend/src/main/java/com/example/demo/exception/AllReadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exception;
+
+public class AllReadyExistsException extends Exception {
+    public AllReadyExistsException(String message) {
+        super(message);
+    }
+}
